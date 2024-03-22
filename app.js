4928b2e3050ea5bd9e8cc24ab7f726b2f5b69c4f
samirvithlani <samir.vithlani83955@gmail.com>
2024-03-22 16:30:58 +0530

    user

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose'); //import mongoose
 
 const userSchema = require('./src/models/UserModel'); //importing the schema
 
-//server creation
 //localhost:3000/users
 app.get("/users",async(req,res)=>{
 
@@ -20,9 +19,53 @@ app.get("/users",async(req,res)=>{
     })
 })
 
+//:id wildcard character
+//localhost:3000/users/y87682uj
+//select * from users where id = 
+//id name email
+
+app.get("/users/:id",async(req,res)=>{
+
+    const id = req.params.id
+    const user  = await userSchema.findById(id)
+    res.json({
+        message:"user fetch successfully",
+        data:user
+    })
+    // console.log("params...",req.params);
+    // console.log(id);
+    // res.send("ok..")
+
+})
+
+app.get("/userbyemail/:email",async(req,res)=>{
+
+
+    const email = req.params.email
+    const user = await userSchema.find({email:req.params.email})
+    console.log(user);
+    if(user && user.length>0){
+        res.json({
+            message:"user fetch successfully",
+            data:user
+        })
+    }
+    else{
+        res.json({
+            message:"user not found",
+            data:[]
+        })
+    }
+    
+
+
+
+})
+
 
 
 //db connection....
+//server creation
 
 const db = mongoose.connect("mongodb://127.0.0.1:27017/club6_1")
 db.then((data)=>{
